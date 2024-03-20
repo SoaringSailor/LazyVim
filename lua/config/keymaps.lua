11ac03b7b602d2414bfd7421d329faed5b25705c
Mika Lagendijk <90611507+SoaringSailor@users.noreply.github.com>
2024-03-20 07:33:30 +0100

    Add my config to lazyvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local command = vim.api.nvim_create_user_command
+
+-- command and mapping to change directory to dir of current buffer
+command("CDC", "cd %:p:h", {})
+map("n", "<leader>bc", "<cmd>CDC<cr>", { noremap = true, silent = false, desc = "Switch to Buffer Directory" })
+
+-- switch start of line and start of line non-blank
+map({ "n", "v", "o" }, "0", "^", { noremap = true, silent = true, desc = "Start of line (non-blank)" })
+map({ "n", "v", "o" }, "^", "0", { noremap = true, silent = true, desc = "Start of line" })
+
+-- mappings to add lines
+map("n", "<A-o>", "m`o<esc>``", { noremap = true, silent = true, desc = "Create line below" })
+map("n", "<A-O>", "m`O<esc>``", { noremap = true, silent = true, desc = "Create line above" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.conceallevel = 0 -- disable conceal i.e. markdown bold
+opt.mouse = "" -- disable mouse support
+opt.mousehide = true -- hide mouse if possible
+opt.wrap = true -- wrap text if longer than window
+opt.linebreak = true -- break lines at better poitions
+opt.breakindent = true -- indent wrapped lines
+opt.splitright = true -- Always vsplit to right
+opt.splitbelow = true -- always hsplit below
